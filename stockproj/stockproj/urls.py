fc9fc0c43f163391380377de4189e883dbf61adc
audrianalinares <audriana_linares@yahoo.com>
2020-07-13 20:53:47 -0500

    linked urls

--- stockproj/stockproj/urls.py
+++ stockproj/stockproj/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('quotes.urls')),
     # Examples:
     # url(r'^$', 'stockproj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),

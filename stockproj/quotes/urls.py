0630f2b98a24c4a5d95bbc5668ff1880d82cbee9
audrianalinares <audriana_linares@yahoo.com>
2020-07-13 22:20:06 -0500

    added bootstrap

--- stockproj/quotes/urls.py
+++ stockproj/quotes/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views 
 
 urlpatterns = [
-    
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about" ),
 ]

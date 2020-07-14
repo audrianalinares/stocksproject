45dcdb895f452322a14629d16a3a4393a32a7bfe
audrianalinares <audriana_linares@yahoo.com>
2020-07-14 00:49:24 -0500

    added ticker

--- stockproj/quotes/views.py
+++ stockproj/quotes/views.py
@@ -4,14 +4,21 @@ def home(request):
     import requests
     import json 
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_2d493df3ace74135846b58fb4fc7eeb9")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker "/quote?token=pk_2d493df3ace74135846b58fb4fc7eeb9")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api' : api})
 
-    return render(request, 'home.html', {'api':api})
+        else:
+            
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
+
+ 
 
 def about(request):
     return render(request, 'about.html', {})

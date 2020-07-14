87232a7dd3bd0b4e3f135f5fede94cb97fa514a9
audrianalinares <audriana_linares@yahoo.com>
2020-07-13 23:55:23 -0500

    loops

--- stockproj/quotes/views.py
+++ stockproj/quotes/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json 
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_2d493df3ace74135846b58fb4fc7eeb9")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})

8d36379e2e0d829ee551a130d36c22c037b15a79
audrianalinares <audriana_linares@yahoo.com>
2020-07-15 22:12:02 -0500

    connecting table

--- stockproj/quotes/views.py
+++ stockproj/quotes/views.py
@@ -34,6 +34,9 @@ def about(request):
 
 #pk_2d493df3ace74135846b58fb4fc7eeb9
 def add_stock(request):
+    import requests
+    import json 
+
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -44,7 +47,18 @@ def add_stock(request):
 
     else:
         ticker = Stock.objects.all()
-        return render(request, 'add_stock.html', {'ticker' : ticker})
+        output = []
+        for ticker_item in ticker:
+
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) "/quote?token=pk_2d493df3ace74135846b58fb4fc7eeb9")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+    
+         return render(request, 'add_stock.html', {'ticker' : ticker, 'output': output})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
